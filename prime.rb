48eb9bd79d7beb644da2550ab13d82371b26788a
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-24 18:29:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   if num <= 1
     return false
   else
-    (2..num).to_a.all? do |possible_factor|
+    (2..num-1).to_a.all? do |possible_factor|
       num % possible_factor != 0
     end
   end
